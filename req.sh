32187a0afe4ace9132401be600297d3ba222e890
apphoru <63139427+apphoru@users.noreply.github.com>
2020-05-20 21:32:02 +0530

    First upload

---
+++ req.sh
@@ -0,0 +1,10 @@
+mkdir Data_Files
+mv zshrc /data/data/com.termux/files/home/c-banner/Data_Files/
+mv bash.bashrc /data/data/com.termux/files/home/c-banner/Data_Files/
+mv motd /data/data/com.termux/files/home/c-banner/Data_Files/
+mv data.sh /data/data/com.termux/files/home/c-banner/Data_Files/
+mv data01 /data/data/com.termux/files/home/c-banner/Data_Files/
+
+cd Data_Files
+bash data.sh
+cd ..
