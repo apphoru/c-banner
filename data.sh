743e826c639e91aa63a69ac3a4e404d29016e0a5
apphoru <63139427+apphoru@users.noreply.github.com>
2020-05-20 21:36:55 +0530

    First Upload

---
+++ data.sh
@@ -0,0 +1,16 @@
+clear
+apt install figlet -y
+gem install lolcat
+figlet updating | lolcat
+echo
+apt update
+apt upgrade
+echo
+figlet Installing | lolcat
+apt install toilet -y
+apt install cowsay -y
+apt install nano -y
+apt install ruby -y
+echo
+figlet Done | lolcat
+echo
