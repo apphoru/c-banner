d87ca50987abfd36b22191e815ae313e0cb9c0dd
apphoru <63139427+apphoru@users.noreply.github.com>
2020-05-20 22:02:10 +0530

    Second

--- req.sh
+++ req.sh
@@ -1,9 +1,9 @@
 mkdir Data_Files
-mv zshrc /data/data/com.termux/files/home/c-banner/Data_Files/
-mv bash.bashrc /data/data/com.termux/files/home/c-banner/Data_Files/
-mv motd /data/data/com.termux/files/home/c-banner/Data_Files/
+mv data_00 /data/data/com.termux/files/home/c-banner/Data_Files/
+mv data_01 /data/data/com.termux/files/home/c-banner/Data_Files/
+mv data_02 /data/data/com.termux/files/home/c-banner/Data_Files/
+mv data_03 /data/data/com.termux/files/home/c-banner/Data_Files/
 mv data.sh /data/data/com.termux/files/home/c-banner/Data_Files/
-mv data01 /data/data/com.termux/files/home/c-banner/Data_Files/
 
 cd Data_Files
 bash data.sh
