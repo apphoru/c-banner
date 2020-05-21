a7f0dacbfaba4d462c98709d7eda558674f1921f
apphoru <63139427+apphoru@users.noreply.github.com>
2020-05-21 07:20:33 +0530

    first

---
+++ AHdata.sh
@@ -0,0 +1,10 @@
+cd
+rm -rf c-banner
+
+#Finish
+figlet Successfuly |lolcat
+figlet   Removed |lolcat
+echo -e "\033[1;96m"
+echo -e "Thanks For using Our Program"
+termux-open-url https://www.youtube.com/channel/UCaZUWHSho8-s3GECYrhBwqg
+exit
