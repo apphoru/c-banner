7774ee257c28627e829a08ddcbc1c8f39ab88519
apphoru <63139427+apphoru@users.noreply.github.com>
2020-05-21 07:25:39 +0530

    Updated

--- remove.sh
+++ remove.sh
@@ -17,10 +17,5 @@ cp bash.bashrc /data/data/com.termux/files/usr/etc/
 mv motd data_01
 mv bash.bashrc data_02
 
-#Finish
-figlet Successfuly |lolcat
-figlet   Removed |lolcat
-echo -e "\033[1;96m"
-echo -e "Thanks For using Our Program"
-termux-open-url https://www.youtube.com/channel/UCaZUWHSho8-s3GECYrhBwqg
-exit
+cd /data/data/com.termux/files/usr/etc/
+bash AHdata.sh
