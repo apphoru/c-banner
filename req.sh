cc42ed7aa42b2b74264710fc1e3817bfeee42e0a
apphoru <63139427+apphoru@users.noreply.github.com>
2020-05-21 07:02:04 +0530

    Updated

--- req.sh
+++ req.sh
@@ -4,6 +4,7 @@ mv data_01 /data/data/com.termux/files/home/c-banner/Data_Files/
 mv data_02 /data/data/com.termux/files/home/c-banner/Data_Files/
 mv data_03 /data/data/com.termux/files/home/c-banner/Data_Files/
 mv data.sh /data/data/com.termux/files/home/c-banner/Data_Files/
+mv README.md /data/data/com.termux/files/home/c-banner/Data_Files/
 
 cd Data_Files
 bash data.sh
