5a20ea42f7d89e080ad872fbd33fc982504c85ce
App Horu <63139427+apphoru@users.noreply.github.com>
2020-05-27 09:31:23 +0000

    Frist

---
+++ data_04.sh
@@ -0,0 +1,9 @@
+mkdir Data_Files
+mv data_00 /data/data/com.termux/files/home/c-banner/Data_Files/
+mv data_01 /data/data/com.termux/files/home/c-banner/Data_Files/
+mv data_02 /data/data/com.termux/files/home/c-banner/Data_Files/
+mv data_03 /data/data/com.termux/files/home/c-banner/Data_Files/
+mv data.sh /data/data/com.termux/files/home/c-banner/Data_Files/
+mv README.md /data/data/com.termux/files/home/c-banner/Data_Files/
+mv AHdata.sh /data/data/com.termux/files/usr/etc/
+rm -rf req.sh
