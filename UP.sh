2aff5ca538bf8fd9e0c03d7fa6f2ba9e6eb9a9a5
App Horu <63139427+apphoru@users.noreply.github.com>
2020-05-27 09:23:35 +0000

    First

---
+++ UP.sh
@@ -0,0 +1,15 @@
+figlet Updating
+cd
+rm -rf c-banner
+cd
+git clone https://github.com/apphoru/c-banner
+clear
+cd c-banner
+cd Data_Files
+bash data_04.sh
+clear
+figlet Updated
+echo "Now You Can Run Program"
+echo "[Just copy paste ]"
+echo
+echo "bash c-banner.sh"
